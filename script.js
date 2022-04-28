ab8a590a1a01a36dac357426eeb1b6068c2b8e92
Herman <hermnchn@gmail.com>
2022-04-27 21:59:06 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ const toggleSwitch = document.querySelector('input[type="checkbox"]');
 
 // Switch Theme Dynamically
 function switchTheme(event){
-    console.log(event);
+    if (event.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }
 }
 
 // Event Listener
