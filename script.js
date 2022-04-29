10ad8f601fb355e62ea3e1b6c0916ee10c616953
Herman <hermnchn@gmail.com>
2022-04-28 23:48:23 -0700

    Update script.js

--- script.js
+++ script.js
@@ -20,9 +20,9 @@ function darkMode() {
 
 // Light Mode Styles
 function darkMode() {
-    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
-    toggleIcon.children[0].textContent = 'Dark Mode';
+    nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    toggleIcon.children[0].textContent = 'Light Mode';
     toggleIcon.children[1].classList.remove('fa-sun');
     toggleIcon.children[1].classList.add('fa-moon');
     image1.src = 'img/undraw_proud_coder_dark.svg';
